4b8852d5e061185b85b34ec148820eb12485dc0d
Aielina Akylbekova <aielina.madambekova@gmail.com>
2024-09-22 18:49:13 +0600

    Add function to generate grid with hidden item

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+const createItems = () =>{
+    const items = Array(36).fill({hasItem: false, checked: false});
+    const randomIndex = Math.floor(Math.random() * 36);
+    items[randomIndex] = {hasItem: true, checked: false};
+    return items;
+}
+
 const Game = () => {
+    const [items, setItems] = useState(createItems());
+
     return (
         <div>
             <h2>Game Component</h2>
         </div>
     );
 };
+
+export default Game;
